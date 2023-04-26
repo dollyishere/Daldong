8b9c69342a994c523abec988f7918562f342d989
dollyishere <rundollyrun8@gmail.com>
2023-04-26 09:59:33 +0900

    Sparkles: exercise_screen 메인 화면 초기 구현 및 캘린더 생성

--- frontend/daldong/lib/screens/home_screen/home_screen.dart
+++ frontend/daldong/lib/screens/home_screen/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:navbar_router/navbar_router.dart';
 import 'package:daldong/widgets/home_screen/info_block.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,30 +7,20 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<NavbarItem> items = [
-      NavbarItem(Icons.home, 'Home', backgroundColor: colors[0]),
-      NavbarItem(Icons.shopping_bag, 'Products', backgroundColor: colors[1]),
-      NavbarItem(Icons.person, 'Me', backgroundColor: colors[2]),
-    ];
-
-    final Map<int, Map<String, Widget>> _routes = {
-      0: {
-        '/': HomeScreen(),
-      },
-    };
-
     return Scaffold(
       body: SafeArea(
         child: Column(
           // mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [InfoBlock(
-            petNumber: 5,
-            nickName: 'jy',
-            playerLevel: 10,
-            playerExp: 1100,
-            playerKcal: 2540,
-          ),],
+          children: [
+            InfoBlock(
+              petNumber: 5,
+              nickName: 'jy',
+              playerLevel: 10,
+              playerExp: 1100,
+              playerKcal: 2540,
+            ),
+          ],
         ),
       ),
     );
