23763f7adbf37ea4b527842668819e5c01d82d5b
alsth4 <alsth4@naver.com>
2023-04-27 15:14:13 +0900

    Lipstick: 워치 이미지 적용

--- frontend/daldong/android/wear/src/main/java/com/ssafy/wear/MainActivity.kt
+++ frontend/daldong/android/wear/src/main/java/com/ssafy/wear/MainActivity.kt
@@ -5,14 +5,11 @@ import android.os.Bundle
 import com.ssafy.wear.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
 }
