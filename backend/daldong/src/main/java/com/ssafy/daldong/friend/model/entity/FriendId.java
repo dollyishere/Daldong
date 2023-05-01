b5e73b504944fc4c3402624930856e419eb5a15d
박서영 <manchott@naver.com>
2023-05-01 11:19:02 +0900

    :recycle: Entity 이름 수정

--- backend/daldong/src/main/java/com/ssafy/daldong/friend/model/entity/FriendId.java
+++ backend/daldong/src/main/java/com/ssafy/daldong/friend/model/entity/FriendId.java
@@ -5,20 +5,20 @@ import java.util.Objects;
 
 public class FriendId implements Serializable {
 
-    private long userId;
-    private long friendId;
+    private long user;
+    private long friend;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         FriendId that = (FriendId) o;
-        return userId == that.userId &&
-                friendId == that.friendId;
+        return user == that.user &&
+                friend == that.friend;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(userId, friendId);
+        return Objects.hash(user, friend);
     }
 }
--- backend/daldong/src/main/java/com/ssafy/daldong/main/model/entity/UserAssetId.java
+++ backend/daldong/src/main/java/com/ssafy/daldong/main/model/entity/UserAssetId.java
@@ -5,20 +5,20 @@ import java.util.Objects;
 
 public class UserAssetId implements Serializable {
 
-    private long userId;
-    private long assetId;
+    private long user;
+    private long asset;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         UserAssetId that = (UserAssetId) o;
-        return userId == that.userId &&
-                assetId == that.assetId;
+        return user == that.user &&
+                asset == that.asset;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(userId, assetId);
+        return Objects.hash(user, asset);
     }
 }
