8adb35360ce005a19ece378b93f4d3e7d9a95d13
전주영[서울_1반_A104]팀원 <dev.jeonjuyoung@outlook.kr>
2023-05-09 11:35:27 +0900

    Revert "Merge branch '34-user' into 'master'"

--- backend/daldong/src/main/java/com/ssafy/daldong/mission/model/repository/DailyMissionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.daldong.mission.model.repository;
-
-import com.ssafy.daldong.mission.model.entity.DailyMission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DailyMissionRepository extends JpaRepository<DailyMission, Long> {
-}
