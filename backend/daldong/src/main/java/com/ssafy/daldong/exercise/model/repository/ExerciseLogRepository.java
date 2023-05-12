98b3297653c3b7c22e930338b9687db3541b2506
전주영[서울_3반_A302]팀원 <wjswndud53@gmail.com>
2023-05-12 16:19:01 +0900

    :fix: repo return값을 Optional로 변경

--- backend/daldong/src/main/java/com/ssafy/daldong/exercise/model/repository/ExerciseLogRepository.java
+++ backend/daldong/src/main/java/com/ssafy/daldong/exercise/model/repository/ExerciseLogRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface ExerciseLogRepository extends JpaRepository<ExerciseLog, Long> {
-    List<ExerciseLog> findByUser_UserIdAndExerciseEndTimeBetween(long user_userId, LocalDateTime exerciseEndTime, LocalDateTime exerciseEndTime2);
+    Optional<List<ExerciseLog>> findByUser_UserIdAndExerciseEndTimeBetween(long user_userId, LocalDateTime exerciseEndTime, LocalDateTime exerciseEndTime2);
 }
