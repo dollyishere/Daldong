107bed3f491bd8b8c5b542a8ed277f0f3d90c761
minsu121x <whalstn1020@hanmail.net>
2023-05-15 15:02:17 +0900

    :hammer: 인벤 페이지 수정

---
+++ backend/daldong/src/main/java/com/ssafy/daldong/main/model/dto/AssetMainDTO.java
@@ -0,0 +1,14 @@
+package com.ssafy.daldong.main.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AssetMainDTO {
+    private long assetId;
+}
