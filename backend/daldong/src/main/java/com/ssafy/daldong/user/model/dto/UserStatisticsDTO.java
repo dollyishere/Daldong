7cce2b65f5edfa7bd2960f69ba5c78091273dd74
조민수 <whalstn1020@hanmail.net>
2023-05-16 10:38:24 +0900

    Resolve "friend request"

---
+++ backend/daldong/src/main/java/com/ssafy/daldong/user/model/dto/UserStatisticsDTO.java
@@ -0,0 +1,42 @@
+package com.ssafy.daldong.user.model.dto;
+
+import com.ssafy.daldong.main.model.dto.AssetDTO;
+import com.ssafy.daldong.user.model.entity.Statistics;
+import com.ssafy.daldong.user.model.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserStatisticsDTO {
+
+    private User user;
+
+    private LocalTime dailyExTime;
+
+    private int dailyKcal;
+
+    private int dailyCount;
+
+    private int dailyFriend;
+
+    private int dailyPoint;
+    public Statistics fromEntity(User user){
+        return Statistics.builder()
+                .user(user)
+                .dailyExTime(LocalTime.of(0,0,0))
+                .dailyKcal(0)
+                .dailyCount(0)
+                .dailyFriend(0)
+                .dailyPoint(0)
+                .build();
+    }
+
+}
+
