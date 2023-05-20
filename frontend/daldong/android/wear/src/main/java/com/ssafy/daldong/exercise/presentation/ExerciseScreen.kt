c5a90e39bc94927714769de77693978f53c6c7ba
김민소 <alsth4@naver.com>
2023-05-20 14:17:30 +0900

    :bug: 버그 수정

--- frontend/daldong/android/wear/src/main/java/com/ssafy/daldong/exercise/presentation/ExerciseScreen.kt
+++ frontend/daldong/android/wear/src/main/java/com/ssafy/daldong/exercise/presentation/ExerciseScreen.kt
@@ -310,54 +310,54 @@ fun ExerciseScreen(
 
                                     println("총 결과 출력 : ${exerciseResult.toString()}")
 
-//                                    navController.navigate(
-//                                        Screens.SummaryScreen.route + "/" +
-//                                                "${tempAverageHeartRate.value.toInt()} bpm/" +
-//                                                "${formatDistanceKm(tempDistance.value)} km/" +
-//                                                "${formatCalories(tempCalories.value)} kcal/" +
-//                                                formatElapsedTime(activeDuration.toKotlinDuration(), true).toString()
-//                                    ) { popUpTo(Screens.ExerciseScreen.route) { inclusive = true } }
+                                    navController.navigate(
+                                        Screens.SummaryScreen.route + "/" +
+                                                "${tempAverageHeartRate.value.toInt()} bpm/" +
+                                                "${formatDistanceKm(tempDistance.value)} km/" +
+                                                "${formatCalories(tempCalories.value)} kcal/" +
+                                                formatElapsedTime(activeDuration.toKotlinDuration(), true).toString()
+                                    ) { popUpTo(Screens.ExerciseScreen.route) { inclusive = true } }
 
-                                    LaunchedEffect(Unit) {
-                                        // Retrofit을 사용한 비동기 호출을 위한 코루틴입니다.
+//                                    LaunchedEffect(Unit) {
+//                                        // Retrofit을 사용한 비동기 호출을 위한 코루틴입니다.
+////
+//                                        try {
+//                                            val response = withContext(Dispatchers.IO) {
+//                                                RetrofitExerciseService().saveExerciseResult(exerciseResult)
+//                                            }
+//                                            // 결과 처리
+//                                            if (response.isSuccessful) {
+//                                                // 성공적인 응답인 경우 (응답 코드가 200인 경우)
+//                                                val responseData = response.body()
+//                                                // responseData를 처리하는 코드 작성
+//                                                Log.d("운동 결과 화면", "운동 결과 전송 성공 ${responseData}")
+//                                            }else {
+//                                                // 서버로부터 에러 응답이 온 경우 (응답 코드가 400 등인 경우)
+//                                                val errorCode = response.code()
+//                                                val errorBody = response.errorBody()?.string()
+//                                                Log.d("운동 결과 화면", "HTTP 요청 실패 - 코드: $errorCode, 에러 메시지: $errorBody")
+//                                                // 에러 처리 로직을 추가하여 사용자에게 알림 또는 적절한 조치를 취할 수 있습니다.
+//                                            }
 //
-                                        try {
-                                            val response = withContext(Dispatchers.IO) {
-                                                RetrofitExerciseService().saveExerciseResult(exerciseResult)
-                                            }
-                                            // 결과 처리
-                                            if (response.isSuccessful) {
-                                                // 성공적인 응답인 경우 (응답 코드가 200인 경우)
-                                                val responseData = response.body()
-                                                // responseData를 처리하는 코드 작성
-                                                Log.d("운동 결과 화면", "운동 결과 전송 성공 ${responseData}")
-                                            }else {
-                                                // 서버로부터 에러 응답이 온 경우 (응답 코드가 400 등인 경우)
-                                                val errorCode = response.code()
-                                                val errorBody = response.errorBody()?.string()
-                                                Log.d("운동 결과 화면", "HTTP 요청 실패 - 코드: $errorCode, 에러 메시지: $errorBody")
-                                                // 에러 처리 로직을 추가하여 사용자에게 알림 또는 적절한 조치를 취할 수 있습니다.
-                                            }
-
-                                        }catch (e: retrofit2.HttpException) {
-                                            // HTTP 요청 실패 처리
-                                            val errorCode = e.code()
-                                            val errorBody = e.response()?.errorBody()?.string()
-                                            Log.d("운동 결과 화면", "HTTP 요청 실패 - 코드: $errorCode, 에러 메시지: $errorBody")
-
-                                        }catch (e: Exception) {
-                                            // 에러 처리
-                                            Log.d("운동 결과 화면", "운동 결과 전송 에러 : ${e.toString()}")
-                                        }
-
-                                        navController.navigate(
-                                            Screens.SummaryScreen.route + "/" +
-                                                    "${tempAverageHeartRate.value.toInt()} bpm/" +
-                                                    "${formatDistanceKm(tempDistance.value)} km/" +
-                                                    "${formatCalories(tempCalories.value)} kcal/" +
-                                                    formatElapsedTime(activeDuration.toKotlinDuration(), true).toString()
-                                        ) { popUpTo(Screens.ExerciseScreen.route) { inclusive = true } }
-                                    }
+//                                        }catch (e: retrofit2.HttpException) {
+//                                            // HTTP 요청 실패 처리
+//                                            val errorCode = e.code()
+//                                            val errorBody = e.response()?.errorBody()?.string()
+//                                            Log.d("운동 결과 화면", "HTTP 요청 실패 - 코드: $errorCode, 에러 메시지: $errorBody")
+//
+//                                        }catch (e: Exception) {
+//                                            // 에러 처리
+//                                            Log.d("운동 결과 화면", "운동 결과 전송 에러 : ${e.toString()}")
+//                                        }
+//
+//                                        navController.navigate(
+//                                            Screens.SummaryScreen.route + "/" +
+//                                                    "${tempAverageHeartRate.value.toInt()} bpm/" +
+//                                                    "${formatDistanceKm(tempDistance.value)} km/" +
+//                                                    "${formatCalories(tempCalories.value)} kcal/" +
+//                                                    formatElapsedTime(activeDuration.toKotlinDuration(), true).toString()
+//                                        ) { popUpTo(Screens.ExerciseScreen.route) { inclusive = true } }
+//                                    }
 
 //                                Button(onClick = { onStartClick() }) {
 //                                    Icon(
