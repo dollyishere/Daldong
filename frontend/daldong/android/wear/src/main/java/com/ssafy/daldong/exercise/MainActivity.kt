20dcff64984729131e8c2612b3bb52cc9c6d930e
김민소 <alsth4@naver.com>
2023-05-21 15:20:48 +0900

    :bug: 버그 수정

--- frontend/daldong/android/wear/src/main/java/com/ssafy/daldong/exercise/MainActivity.kt
+++ frontend/daldong/android/wear/src/main/java/com/ssafy/daldong/exercise/MainActivity.kt
@@ -47,19 +47,59 @@ class MainActivity : ComponentActivity() {
         storeUserManager = StoreUserManager(dataStore) // 싱글톤
 
         lifecycleScope.launch {
+            /** Check if we have an active exercise. If true, set our destination as the
+             * Exercise Screen. If false, route to preparing a new exercise. **/
+
+            // Room 데이터베이스 인스턴스 초기화
+//            val db = Room.databaseBuilder(applicationContext, ExerciseResultDatabase::class.java, "exercise_result").build()
+//            val myDao = db.exerciseResultDao()
+//
+//            lifecycleScope.launch(Dispatchers.IO) {
+//                Log.d(TAG, "db 저장된 값 ${myDao.getAll().toString()}")
+//            }
+
             //주위에 도달할 노드 없는경우
-            val destination = when (exerciseViewModel.isExerciseInProgress()) {
-                false -> Screens.StartingUp.route
-                true -> Screens.ExerciseScreen.route
-            }
+            val nodes = getCapabilitiesForReachableNodes()
+            if (nodes.isEmpty()) {
+                // 노드가 없음
+                Log.d(TAG, "도달 가능한 폰 없음")
+                setContent{
+                    NoReachableNodes()
+                }
 
-            setContent {
-                navController = rememberSwipeDismissableNavController()
+            } else {
+                // 노드가 있음
+                Log.d(TAG, "도달 가능한 폰 있음")
+                // capabilities.values()를 사용하여 노드의 기능 목록을 얻을 수 있습니다.
+                // capabilities.keys()를 사용하여 노드 목록을 얻을 수 있습니다.
 
-                ExerciseWearApp(
-                    navController,
-                    startDestination = destination,
-                )
+                // 블루 투스 방식 X, app 프로젝트 wear.xml 때문에 연결 가능한 것 같음
+                // app을 지우면 폰 목록 안뜸
+
+                Log.d(TAG, "도달 가능한 폰 목록 : ${nodes.keys}")
+                Log.d(TAG, "도달 가능한 폰 기능 목록 : ${nodes.values}")
+
+                Log.d(TAG, "유저아이디: ${userInfoViewModel.userId.value}")
+
+                if (userInfoViewModel.userId.value.isNullOrEmpty() ){
+                    setContent{
+                        InvalidUser()
+                    }
+                }else{
+                    val destination = when (exerciseViewModel.isExerciseInProgress()) {
+                        false -> Screens.StartingUp.route
+                        true -> Screens.ExerciseScreen.route
+                    }
+
+                    setContent {
+                        navController = rememberSwipeDismissableNavController()
+
+                        ExerciseWearApp(
+                            navController,
+                            startDestination = destination,
+                        )
+                    }
+                }
             }
         }
     }
